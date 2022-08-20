14c073e4aeecebaf8e4c8c9843c512d47a0ef815
juniorandrades <juniorandrades2000@gmail.com>
2022-08-20 18:14:40 -0300

    Implementação do DoCheck

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit, } from '@angular/core';
 
 //Interface
 import { TaskList } from '../../model/task-list';
@@ -8,17 +8,19 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array <TaskList> = [
     
   ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngDoCheck (){
+    this.taskList.sort ((first, last) => Number(first.checked) - Number(last.checked))
   }
 
+  constructor() { }
+
+ 
 
   public setEmitTaskList(event: string){
     this.taskList.push ({task: event, checked: false});
@@ -34,5 +36,17 @@ export class TodoListComponent implements OnInit {
     if (confirm) {
     this.taskList = [];
     }
+  }
+
+  public validationInput (event: string, index: number)  {
+
+    if(!event.length) {
+      const confirm = window.confirm("Task está vazia, deseja deleta-la?");
+
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+
   }
 }
