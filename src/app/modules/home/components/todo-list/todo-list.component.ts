27cae06e09b99369a73a94d373392f0358bbf23c
juniorandrades <juniorandrades2000@gmail.com>
2022-08-20 11:50:18 -0300

    Interface/Delete function adicionadas

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array <TaskList> = [
+    {task:"Minha nova Task", checked: true},
+    {task:"Minha nova Task 2", checked: false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public deleteItemTaskList (event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList (){
+    const  confirm = window.confirm("Você deseja realmente Deletar tudo?")
+
+    if (confirm) {
+    this.taskList = [];
+    }
+  }
 }
